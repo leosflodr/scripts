6844f6831369b0f782a93d60afea796e86c61dfc
leos.flodr <leos.flodr@gooddata.com>
2011-11-16 16:34:42 +0100

    posledni podoba

--- create_pardot_integration.sh
+++ create_pardot_integration.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # This script take its parameters and use them for inserting nev project in connectors database.  
+# ASSUMPTION - sql skript insert-pardot-integration.sql is on the same directory as this script. In other case you should edit variable insert_sgl
+
 
 set -e
+export work_dir=`dirname $0`
+export insert_sql=${work_dir}/insert-pardot-integration.sql
+
 
 insert_new_project () {
 if [ "x$1" == "x" ]; then 
@@ -14,10 +19,8 @@ fi
 
 echo "will create project $proj" 
 
-
-
-echo mysql -uroot -p`cat /etc/gdc/etc/mysql ` connectors -e "set @project_uri:=/gdc/projects/$proj; set @account_id:='5552'; source insert-pardot-integration.sql;"
-
+echo mysql -uroot -p`cat /etc/gdc/etc/mysql ` connectors -e "set @project_uri:='/gdc/projects/$proj'; set @account_id:='5552'; source ${insert_sql};"
+mysql -uroot -p`cat /etc/gdc/etc/mysql ` connectors -e "set @project_uri:='/gdc/projects/$proj'; set @account_id:='5552'; source ${insert_sql};"
 
 }
 
@@ -25,8 +28,6 @@ echo mysql -uroot -p`cat /etc/gdc/etc/mysql ` connectors -e "set @project_uri:=/
 # main body
 
 export projects=$@
-
-echo toto jsou parametry $projects
 
 for pr in $projects ; do
   insert_new_project $pr;
